2657464dd562daac60f1c608a8147facc94c184e
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-04 00:30:20 +0600

    signup & signin

--- bookmark-api/src/auth/auth.controller.ts
+++ bookmark-api/src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+import { SigninDto, SingupDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  singup(@Body() dto: AuthDto) {
+  singup(@Body() dto: SingupDto) {
     return this.authService.signup(dto);
   }
 
   @Post('/signin')
-  signin() {
-    return this.authService.signin();
+  signin(@Body() dto: SigninDto) {
+    return this.authService.signin(dto);
   }
 }
--- bookmark-api/src/auth/dto/auth.dto.ts
+++ bookmark-api/src/auth/dto/auth.dto.ts
@@ -1,12 +1,22 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class AuthDto {
+export class SingupDto {
   @IsNotEmpty()
   first_name: string;
 
   @IsNotEmpty()
   last_name: string;
 
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class SigninDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
