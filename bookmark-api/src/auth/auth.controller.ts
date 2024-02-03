d02c6f4d6b3517d7d4d70f9a85ee0c9f9e9e0a33
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-03 16:46:19 +0600

    first request

---
+++ bookmark-api/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  singup() {
+    return {
+      message: 'I am signed up',
+    };
+  }
+
+  @Post('/signin')
+  signin() {
+    return 'I am signed in';
+  }
+}
