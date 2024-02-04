7bb0e35ff20914fe352a77ae7b3f419b21664f7e
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-04 22:17:36 +0600

    nestjs jwt

--- bookmark-api/src/user/user.controller.ts
+++ bookmark-api/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
-@Controller('user')
-export class UserController {}
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
