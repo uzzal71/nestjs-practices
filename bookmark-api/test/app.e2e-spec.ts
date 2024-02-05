82908d6c628441b153f443acda97b0a5a7314ba4
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-05 23:59:56 +0600

    nestjs testing

--- bookmark-api/test/app.e2e-spec.ts
+++ bookmark-api/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as pactum from 'pactum';
 import { AppModule } from '../src/app.module';
+import { AuthDto } from '../src/auth/dto';
 import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App e2e', () => {
@@ -11,7 +12,8 @@ describe('App e2e', () => {
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })import { AuthDto } from '../../nestjs-api-tutorial/src/auth/dto/auth.dto';
+.compile();
 
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(
@@ -33,7 +35,15 @@ describe('App e2e', () => {
 
   describe('Auth', () => {
     describe('Signup', () => {
-      it.todo('should sign up');
+      it('should signup', () => {
+        const dto: AuthDto = {
+          email: "email@example.com",
+          hash: "password",
+          first_name: "uzzal",
+          last_name: "roy"
+        }
+        return pactum.spec().post('http://localhost:3333/auth/signup').withBody(dto).expectStatus(201);
+      });
     });
 
     describe('Signin', () => {
