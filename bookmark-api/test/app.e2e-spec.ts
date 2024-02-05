0748c0a7d4487c2c3577ad61c31ff2944a037646
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-06 00:11:34 +0600

    e2e

--- bookmark-api/test/app.e2e-spec.ts
+++ bookmark-api/test/app.e2e-spec.ts
@@ -59,14 +59,23 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
 
   describe('User', () => {
     describe('Get me', () => {
-      it.todo('should get me');
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
     });
 
     describe('Edit user', () => {
